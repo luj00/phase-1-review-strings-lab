4811e2eb23f7d6f74b78811b930be3f70e55a03b
root <root@DESKTOP-598GE48.localdomain>
2023-09-28 15:17:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+// index.js
+
+// Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// Export the variables
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting,
+};
+
